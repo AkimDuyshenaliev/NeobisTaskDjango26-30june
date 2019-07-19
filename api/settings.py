ce1618357e627774644814878b653851e8c21a35
AkimDuyshenaliev <akim.duyshenaliev@gmail.com>
2019-07-19 14:59:28 +0600

    Added some fixes

--- api/settings.py
+++ api/settings.py
@@ -13,14 +13,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import dj_database_url
 from decouple import config, Csv
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'api.settings'
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'api.settings'
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -32,9 +27,6 @@ MEDIA_URL = "/media/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = config('SECRET_KEY')
